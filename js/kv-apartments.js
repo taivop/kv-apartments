5747a5bb46aee472677d75fcee1854abb44ca0df
taivop <taivo.pungas@gmail.com>
2015-08-18 18:18:22 +0300

    #2 dropdowns for apartment state and part of city

--- js/kv-apartments.js
+++ js/kv-apartments.js
@@ -1,11 +1,32 @@
 
 
 ready = function(error, rows) {
-    console.log(rows);
-    // Generate a Bates distribution of 10 random variables.
-    var values = rows;
 
-// A formatter for counts.
+    // Populate form with options
+    features = {};
+    features.parts_of_city = d3.map(rows, function(d){ return d.Linnaosa; }).keys().sort();
+    features.parts_of_city.unshift("[Linnaosa]");
+    features.apartment_states = d3.map(rows, function(d){ return d.Seisukord; }).keys().sort();
+    features.apartment_states.unshift("[Korteri seisukord]");
+
+    d3.select("select#part_of_city")
+        .selectAll("option")
+        .data(features.parts_of_city)
+        .enter()
+        .append("option")
+        .attr("value", function(d) { return d; })
+        .text(function(d) { return d; })
+    
+
+    d3.select("select#apartment_state")
+        .selectAll("option")
+        .data(features.apartment_states)
+        .enter()
+        .append("option")
+        .attr("value", function(d) { return d; })
+        .text(function(d) { return d; })
+
+    // A formatter for counts.
     var formatCount = d3.format(",.0f");
 
     var margin = {top: 10, right: 30, bottom: 30, left: 30},
@@ -16,11 +37,11 @@ ready = function(error, rows) {
         .domain([0, Math.max(300000, d3.min(rows, function(d) { return d.Hind }))])
         .range([0, width]);
 
-// Generate a histogram using twenty uniformly-spaced bins.
+    // Generate a histogram using twenty uniformly-spaced bins.
     var data = d3.layout.histogram()
         .bins(x.ticks(20))
         .value(function(d) { return d.Hind; })
-    (values);
+    (rows);
     var y = d3.scale.linear()
         .domain([0, d3.max(data, function(d) { return d.y; })])
         .range([height, 0]);
@@ -57,12 +78,30 @@ ready = function(error, rows) {
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
+
+    // Update when inputs change
+    d3.select("select#part_of_city").on("input", function() {
+        console.log(this.value)
+    });
 }
 
 
 
+rowParser = function(d) {
+    return {
+        ID: d.ID,
+        Hind: parseFloat(d.HindKohandatud),
+        Linnaosa: d.Linnaosa,
+        Üldpind: d.Üldpind,
+        Seisukord: d.Seisukord,
+        Tube: parseInt(d.Tube),
+        Korrus: parseInt(d.Korrus),
+        Korruseid: parseInt(d.Korruseid)
+    };
+}
+
 // Load data
 var dsv = d3.dsv(";", "text/plain");
-dsv("data/apartment_sell_tallinn.csv")
-    .row(function(d) { return {ID: d.ID, Hind: parseFloat(d.Hind)}; })
+dsv("data/apartment_sell_tallinn_test.csv")
+    .row(rowParser)
     .get(ready);
